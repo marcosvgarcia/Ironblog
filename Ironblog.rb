caf8abe3b38582b922e6eff3d8811c455a610b1c
Marcos Vazquez <marcosvazquezgarcia@outlook.es>
2015-04-12 18:49:23 +0200

    Estructura
    Creo dos clases: Blog y Post
    
    Defino los constructores con sus argumentos correspondientes

--- Ironblog.rb
+++ Ironblog.rb
@@ -1,15 +1,15 @@
-require "rubygems"
-
-class Blog
-	def array.each do
-	Post
-	end
-end
 
 class Post
+
+	attr_accessor :title, :date, :text
+
 	def initialize(title, date, text)
 	@title = title
 	@date = date
 	@text = text
 	end
 end
+
+Post1 = Post.new("title1", "12/4/2015", "text1")
+
+puts Post1
